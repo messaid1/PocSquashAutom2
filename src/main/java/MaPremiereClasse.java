bf423a11d6a8b64d687f13f4bcb83b1f1605d10f
Mehdi ESSAID <messaid@norsys.fr>
2022-12-13 19:34:30 +0100

    ajout @SpringBootApplication et main

--- src/main/java/MaPremiereClasse.java
+++ src/main/java/MaPremiereClasse.java
@@ -1,2 +1,8 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MaPremiereClasse {
+    public static void main(String[] args){
+
+    }
 }
